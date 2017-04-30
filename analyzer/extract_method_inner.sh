ad4dc2ae105c32b815d7ad2ddfd5fe64aa32e049
perfprobe <perfprobe@gmail.com>
2017-04-30 14:58:55 -0400

    Create extract_method_inner.sh

---
+++ analyzer/extract_method_inner.sh
@@ -0,0 +1,16 @@
+id=$2
+rm methods.inner.$1
+c1=$(grep -n "PerformClick.run" $tid.$1.*traceview | head -n1 | cut -d':' -f1)
+c2=$(grep -n "performTraversals" $tid.$1.*traceview | tail -n1 | cut -d':' -f1)
+for f in $(cat $tid.$1.*traceview | head -n$c2 | tail -n$(($c2-$c1+1)) | sed -e 's/^ *//g;s/ *$//g' | grep " xit " | cut -d' ' -f1 | sort | uniq);
+do
+   if [ $f -lt 10 ]; then
+      cat $tid.$1.*traceview | head -n$c2 | tail -n$(($c2-$c1+1)) | grep " $f ent\| $f xit" > $f.$1.out
+   else
+      cat $tid.$1.*traceview | head -n$c2 | tail -n$(($c2-$c1+1)) | grep "$f ent\|$f xit" > $f.$1.out
+   fi
+done
+for f in $(ls *.$1.out);
+do
+    cat $f | python innerMethod.py >> methods.inner.$1
+done
