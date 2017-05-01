bc3adae883dd7a20b6be2f05be48405e11230be1
perfprobe <perfprobe@gmail.com>
2017-05-01 17:17:27 -0400

    Update trace_collect.sh

--- tester/trace_collect.sh
+++ tester/trace_collect.sh
@@ -1,3 +1,6 @@
 # Pull traces from a mobile device
 adb pull /sdcard/*.logcat .
 adb shell ls /sdcard/*.tracedump | tr '\r' ' ' | xargs -n1 adb pull
+
+# Compute delay for each run and parse function call trace
+n=1; for f in $(cat traceview.info); do tid=$(echo $f | cut -d'.' -f1); c=$(cat app.latency | head -n$n | tail -n1 | cut -d',' -f1); ./dmtracedump -o $f > $tid.$c.traceview; n=$(($n+1)); done 
