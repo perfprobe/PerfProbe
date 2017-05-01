e8f23162b1e7c9a18f7d8b0e98aecb839949332b
perfprobe <perfprobe@gmail.com>
2017-05-01 18:30:57 -0400

    Update trace_process.sh

--- tester/trace_process.sh
+++ tester/trace_process.sh
@@ -5,7 +5,7 @@ adb shell ls /sdcard/*.tracedump | tr '\r' ' ' | xargs -n1 adb pull
 # Collect Panappticon trace from a server and parse it
 mkdir user
 mkdir kernel
-
+./parse_event.sh
 
 # Compute delay for each run and parse function call trace
 n=1; for f in $(cat traceview.info); do tid=$(echo $f | cut -d'.' -f1); c=$(cat app.latency | head -n$n | tail -n1 | cut -d',' -f1); ./dmtracedump -o $f > $tid.$c.traceview; n=$(($n+1)); done 
