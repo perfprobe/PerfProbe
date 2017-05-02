3bfaefd2b50a3ace309340af0295c3c205384547
perfprobe <perfprobe@gmail.com>
2017-05-01 21:43:31 -0400

    Update start_analyzer.sh

--- analyzer/start_analyzer.sh
+++ analyzer/start_analyzer.sh
@@ -2,4 +2,7 @@
 for f in $(ls *.traceview); do tid=$(echo $f | cut -d'.' -f1); i=$(echo $f | cut -d'.' -f2); ./extract_inner_method.sh $i $tid; done
 for f in $(ls *.traceview); do tid=$(echo $f | cut -d'.' -f1); i=$(echo $f | cut -d'.' -f2); ./extract_method.sh $i $tid; done 
 
-# 
+# Prune to get top-K methods for DT selection
+./get_topk_method.sh $app
+
+# DT selection on function features
