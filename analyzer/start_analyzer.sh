a2127698a89cf6311e407683b415c51865555cc8
perfprobe <perfprobe@gmail.com>
2017-05-01 23:11:42 -0400

    Update start_analyzer.sh

--- analyzer/start_analyzer.sh
+++ analyzer/start_analyzer.sh
@@ -6,3 +6,8 @@ for f in $(ls *.traceview); do tid=$(echo $f | cut -d'.' -f1); i=$(echo $f | cut
 ./get_topk_method.sh $app
 
 # DT selection on function features
+echo -n "label" > feature.meta; for l in $(cat method_feature.meta); do echo -n ",$l" >> feature.meta; done
+for l in $(cut -d',' -f2 *.latency); do if [ $l -gt $thres ]; then echo "-1" >> tmp; else echo "+1" >> tmp; fi; done
+paste tmp method_feature.val > dtree.csv
+sed -i 's/\t//g' dtree.csv; rm tmp
+python dtree.py dtree.csv
