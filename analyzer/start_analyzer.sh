7049f7640140c9eb1d214fca3cf75afb9174a303
perfprobe <perfprobe@gmail.com>
2017-05-01 23:16:14 -0400

    Update start_analyzer.sh

--- analyzer/start_analyzer.sh
+++ analyzer/start_analyzer.sh
@@ -1,13 +1,25 @@
-# Extract method feature from function call trace
+# Extract inner method feature from function call trace
 for f in $(ls *.traceview); do tid=$(echo $f | cut -d'.' -f1); i=$(echo $f | cut -d'.' -f2); ./extract_inner_method.sh $i $tid; done
-for f in $(ls *.traceview); do tid=$(echo $f | cut -d'.' -f1); i=$(echo $f | cut -d'.' -f2); ./extract_method.sh $i $tid; done 
 
-# Prune to get top-K methods for DT selection
-./get_topk_method.sh $app
+# Prune to get top-K inner methods for DT selection
+./get_topk_method.sh $k $app
 
 # DT selection on function features
 echo -n "label" > feature.meta; for l in $(cat method_feature.meta); do echo -n ",$l" >> feature.meta; done
 for l in $(cut -d',' -f2 *.latency); do if [ $l -gt $thres ]; then echo "-1" >> tmp; else echo "+1" >> tmp; fi; done
 paste tmp method_feature.val > dtree.csv
+sed -i 's/\t//g' dtree.csv; rm tmp
+python dtree.py dtree.csv
+
+# Extract method feature from function call trace
+for f in $(ls *.traceview); do tid=$(echo $f | cut -d'.' -f1); i=$(echo $f | cut -d'.' -f2); ./extract_method.sh $i $tid; done 
+
+# Prune to get top-K methods for DT selection
+./get_topk_all_method.sh $k $app
+
+# DT selection on function features
+echo -n "label" > feature.meta; for l in $(cat method_all_feature.meta); do echo -n ",$l" >> feature.meta; done
+for l in $(cut -d',' -f2 *.latency); do if [ $l -gt $thres ]; then echo "-1" >> tmp; else echo "+1" >> tmp; fi; done
+paste tmp method_all_feature.val > dtree.csv
 sed -i 's/\t//g' dtree.csv; rm tmp
 python dtree.py dtree.csv
