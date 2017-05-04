90d49fa26e66dfa670484ea97b429ae268566b41
perfprobe <perfprobe@gmail.com>
2017-05-04 17:08:08 -0400

    Update profile_resource.sh

--- analyzer/profile_resource.sh
+++ analyzer/profile_resource.sh
@@ -4,7 +4,11 @@ cut -d'{' -f3 $1.tmp | cut -d':' -f3 | cut -d'}' -f1 > $1.tmp.2
 cut -d'{' -f4 $1.tmp | cut -d':' -f2 | cut -d',' -f1 > $1.tmp.3
 cut -d'{' -f4 $1.tmp | cut -d':' -f3 | cut -d',' -f1 > $1.tmp.4
 cut -d'{' -f4 $1.tmp | cut -d':' -f4 | cut -d'}' -f1 | cut -d'"' -f2 > $1.tmp.5
-paste -d',' $1.tmp.1 $1.tmp.2 $1.tmp.3 $1.tmp.4 $1.tmp.5 > $1.cpu
+cut -d':' -f6 $1.tmp | cut -d',' -f1 > $1.tmp.6
+paste -d',' $1.tmp.1 $1.tmp.2 $1.tmp.3 $1.tmp.4 $1.tmp.5 $1.tmp.6 > $1.cpu
+k=$(echo $1 | cut -d'.' -f2)
+cat $1.cpu | python extractCPUFreq.py trace.$k.cpufreq > $1.cpu.tmp
+mv $1.cpu.tmp $1.cpu
 cat $1 | grep SOCK > $1.tmp
 cut -d'"' -f4 $1.tmp > $1.tmp.1
 cut -d'{' -f3 $1.tmp | cut -d':' -f2 | cut -d',' -f1 > $1.tmp.2
